b0c933ace7a8297ae15682f9203f7277b7842580
Nicolas <55542853+nico5997@users.noreply.github.com>
2020-01-21 01:00:38 +0100

    Add files via upload

---
+++ Deliverable_3_2/Deliverable_3_2.m
@@ -0,0 +1,236 @@
+clear all, close all, clc
+
+%% Part 3
+
+Ts = 1/5;
+quad = Quad(Ts);
+[xs, us] = quad.trim();
+sys = quad.linearize(xs, us);
+[sys_x, sys_y, sys_z, sys_yaw] = quad.decompose(sys, xs, us);
+
+N = 100;
+Work_consumption_x = 0;
+Work_consumption_y = 0;
+Work_consumption_z = 0;
+Work_consumption_yaw = 0;
+
+global tmp;
+tmp = 0;
+
+x = zeros(4,N);
+ux = zeros(1,N);
+ux_tmp = zeros(1,N);
+x(:,1) = [0 0 0 0]';
+y = zeros(4,N);
+uy = zeros(1,N);
+uy_tmp = zeros(1,N);
+y(:,1) = [0 0 0 0]';
+z = zeros(2,N);
+uz = zeros(1,N);
+uz_tmp = zeros(1,N);
+z(:,1) = [0 0]';
+yaw = zeros(2,N);
+uyaw = zeros(1,N);
+uyaw_tmp = zeros(1,N);
+yaw(:,1) = [0 0]';
+
+while (tmp < 2)
+    mpc_x = MPC_Control_x(sys_x, Ts);
+    mpc_y = MPC_Control_y(sys_y, Ts);
+    mpc_z = MPC_Control_z(sys_z, Ts);
+    mpc_yaw = MPC_Control_yaw(sys_yaw, Ts);
+    
+    for i = 1:N-1
+
+        % Extract the optimal input
+        ux(:,i) = mpc_x.get_u(x(:,i), -2);
+        uy(:,i) = mpc_y.get_u(y(:,i), -2);
+        uz(:,i) = mpc_z.get_u(z(:,i), -2);
+        uyaw(:,i) = mpc_yaw.get_u(yaw(:,i), pi/4);
+
+        if (tmp == 0)
+           ux_tmp(:,i) = mpc_x.get_u(x(:,i), -2);
+           uy_tmp(:,i) = mpc_y.get_u(y(:,i), -2);
+           uz_tmp(:,i) = mpc_z.get_u(z(:,i), -2);
+           uyaw_tmp(:,i) = mpc_yaw.get_u(yaw(:,i), pi/4); 
+        end
+
+        if (i ~= 1)
+            Work_consumption_x = Work_consumption_x + abs(ux(1,i)*(x(4,i)-x(4,i-1)));
+            Work_consumption_y = Work_consumption_y + abs(uy(1,i)*(y(4,i)-y(4,i-1)));
+            Work_consumption_z = Work_consumption_z + abs(uz(1,i)*(z(2,i)-z(2,i-1)));
+            Work_consumption_yaw = Work_consumption_yaw + abs(uyaw(1,i)*(yaw(2,i)-yaw(2,i-1)));
+        end
+    i
+        % Apply the optimal input to the system
+        x(:,i+1) = mpc_x.A*x(:,i) + mpc_x.B*ux(:,i);
+        y(:,i+1) = mpc_y.A*y(:,i) + mpc_y.B*uy(:,i);
+        z(:,i+1) = mpc_z.A*z(:,i) + mpc_z.B*uz(:,i);
+        yaw(:,i+1) = mpc_yaw.A*yaw(:,i) + mpc_yaw.B*uyaw(:,i);
+    end
+    tmp = tmp + 1;
+end
+
+% Plots of each dimentions
+
+figure
+grid on
+
+subplot(411);
+plot(Ts*(1:N),x(4,:));
+ylabel('Position x (m)');
+xlabel('time [s]');
+title('Position x of Quadcopter with respect to time')
+
+subplot(412);
+plot(Ts*(1:N),y(4,:));
+ylabel('Position y [m]');
+xlabel('time [s]');
+title('Position y of Quadcopter with respect to time')
+
+subplot(413);
+plot(Ts*(1:N),z(2,:));
+ylabel('Position z [m]');
+xlabel('time [s]');
+title('Position z of Quadcopter with respect t0 time')
+
+subplot(414);
+plot(Ts*(1:N),yaw(2,:));
+ylabel('Angle yaw [rad]');
+xlabel('time [s]');
+title('Angle yaw of Quadcopter with respect to time')
+
+S_x = stepinfo(x(4,:),Ts*(1:N),-2,'SettlingTimeThreshold',0.02);
+disp('Settling time of x is:')
+S_x.SettlingTime
+S_y = stepinfo(y(4,:),Ts*(1:N),-2,'SettlingTimeThreshold',0.02);
+disp('Settling time of y is:')
+S_y.SettlingTime
+S_z = stepinfo(z(2,:),Ts*(1:N),-2,'SettlingTimeThreshold',0.02);
+disp('Settling time of z is:')
+S_z.SettlingTime
+S_yaw = stepinfo(yaw(2,:),Ts*(1:N),pi/4,'SettlingTimeThreshold',0.02);
+disp('Settling time of yaw is:')
+S_yaw.SettlingTime
+
+% Plots of contraints
+
+% Constraints over the input
+figure
+grid on
+
+subplot(411);
+plot(Ts*(1:N),0.3*ones(1,length(ux(1,:))),'r--');
+hold on
+plot(Ts*(1:N),-0.3*ones(1,length(ux(1,:))),'r--');
+plot(Ts*(1:N),ux(1,:));
+ylabel('Moment M\_beta [Nm]');
+xlabel('time [s]');
+axis([1/5 10 -0.4 0.4]);
+title('Input moment M\_beta of Quadcopter with respect to time')
+legend('Contraints')
+
+subplot(412);
+plot(Ts*(1:N),0.3*ones(1,length(ux(1,:))),'r--');
+hold on
+plot(Ts*(1:N),-0.3*ones(1,length(ux(1,:))),'r--');
+plot(Ts*(1:N),uy(1,:));
+ylabel('Moment M\_alpha [Nm]');
+xlabel('time [s]');
+axis([1/5 10 -0.4 0.4]);
+title('Input moment M\_alpha of Quadcopter with respect to time')
+legend('Contraints')
+
+subplot(413);
+plot(Ts*(1:N),0.3*ones(1,length(ux(1,:))),'r--');
+hold on
+plot(Ts*(1:N),-0.2*ones(1,length(ux(1,:))),'r--');
+plot(Ts*(1:N),uz(1,:));
+ylabel('Force F [N]');
+xlabel('time [s]');
+axis([1/5 10 -0.4 0.4]);
+title('Input force F of Quadcopter with respect to time')
+legend('Contraints')
+
+subplot(414);
+plot(Ts*(1:N),0.2*ones(1,length(ux(1,:))),'r--');
+hold on
+plot(Ts*(1:N),-0.2*ones(1,length(ux(1,:))),'r--');
+plot(Ts*(1:N),uyaw(1,:));
+ylabel('Moment M\_gamma [Nm]');
+xlabel('time [s]');
+axis([1/5 10 -0.4 0.4]);
+title('Input moment M\_gamma of Quadcopter with respect to time')
+legend('Contraints')
+
+% Comparison between u and u_optimized
+
+figure
+grid on
+
+subplot(411);
+plot(Ts*(1:N),ux_tmp(1,:));
+hold on
+plot(Ts*(1:N),ux(1,:));
+ylabel('Moment M\_beta [Nm]');
+xlabel('time [s]');
+axis([1/5 10 -0.4 0.4]);
+title('Input moment M\_beta of Quadcopter with respect to time')
+legend('input','optimized input')
+
+subplot(412);
+plot(Ts*(1:N),uy_tmp(1,:));
+hold on
+plot(Ts*(1:N),uy(1,:));
+ylabel('Moment M\_alpha [Nm]');
+xlabel('time [s]');
+axis([1/5 10 -0.4 0.4]);
+title('Input moment M\_alpha of Quadcopter with respect to time')
+legend('input','optimized input')
+
+subplot(413);
+plot(Ts*(1:N),uz_tmp(1,:));
+hold on
+plot(Ts*(1:N),uz(1,:));
+ylabel('Force F [N]');
+xlabel('time [s]');
+axis([1/5 10 -0.4 0.4]);
+title('Input force F of Quadcopter with respect to time')
+legend('input','optimized input')
+
+subplot(414);
+plot(Ts*(1:N),uyaw_tmp(1,:));
+hold on
+plot(Ts*(1:N),uyaw(1,:));
+ylabel('Moment M\_gamma [Nm]');
+xlabel('time [s]');
+axis([1/5 10 -0.4 0.4]);
+title('Input moment M\_gamma of Quadcopter with respect to time')
+legend('input','optimized input')
+
+% constraints over the states
+
+figure
+grid on
+
+subplot(211);
+plot(Ts*(1:N),0.035*ones(1,length(ux(1,:))),'r--');
+hold on
+plot(Ts*(1:N),-0.035*ones(1,length(ux(1,:))),'r--');
+plot(Ts*(1:N),x(2,:));
+ylabel('Angle beta [rad]');
+xlabel('time [s]');
+axis([1/5 10 -0.05 0.05]);
+title('Angles beta of Quadcopter with respect to time')
+legend('Contraints')
+
+subplot(212);
+plot(Ts*(1:N),0.035*ones(1,length(ux(1,:))),'r--');
+hold on
+plot(Ts*(1:N),-0.035*ones(1,length(ux(1,:))),'r--');
+plot(Ts*(1:N),y(2,:));
+ylabel('Angle alpha [rad]');
+xlabel('time [s]');
+axis([1/5 10 -0.05 0.05]);
+title('Angles alpha of Quadcopter with respect to time')
+legend('Contraints')
---
+++ Deliverable_3_2/MPC_Control_z.m
@@ -0,0 +1,197 @@
+classdef MPC_Control_z < MPC_Control
+  properties
+    A_bar, B_bar, C_bar % Augmented system for disturbance rejection    
+    L                   % Estimator gain for disturbance rejection
+  end
+  
+  methods
+    function mpc = MPC_Control_z(sys, Ts)
+      mpc = mpc@MPC_Control(sys, Ts);
+      
+      [mpc.A_bar, mpc.B_bar, mpc.C_bar, mpc.L] = mpc.setup_estimator();
+    end
+    
+    % Design a YALMIP optimizer object that takes a steady-state state
+    % and input (xs, us) and returns a control input
+    function ctrl_opt = setup_controller(mpc)
+
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      % INPUTS
+      %   x(:,1) - initial state (estimate)
+      %   d_est  - disturbance estimate
+      %   xs, us - steady-state target
+      % OUTPUTS
+      %   u(:,1) - input to apply to the system
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+      [n,m] = size(mpc.B);
+      
+      % Steady-state targets (Ignore this before Todo 3.3)
+      xs = sdpvar(n, 1);
+      us = sdpvar(m, 1);
+      
+      % Disturbance estimate (Ignore this before Part 5)
+      d_est = sdpvar(1);
+
+      % SET THE HORIZON HERE
+      N = 10;
+      
+      % Predicted state and input trajectories
+      x = sdpvar(n, N);
+      u = sdpvar(m, N-1);
+      
+
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE 
+
+      % NOTE: The matrices mpc.A, mpc.B, mpc.C and mpc.D are 
+      %       the DISCRETE-TIME MODEL of your system
+
+      % SET THE PROBLEM CONSTRAINTS con AND THE OBJECTIVE obj HERE
+      con = [];
+      obj = 0;
+      % SET PARAMETERS HERE
+      global tmp;
+      
+      if (tmp == 0)
+          Q = eye(n);
+          Q(n,n) = 1;
+          R = 1*eye(m);
+      end
+      
+      if (tmp == 1)
+          Q = [20 0;
+               0 800];
+          R = 1*eye(m);
+
+      end
+      
+      % CONSTRAINTS
+      %  u in U = { u | Mu <= m }
+      M = [1;-1]; m = [0.3; 0.2];
+      % x in X = { x | Fx <= f }
+      F = [0 0; 0 0]; f = [0; 0];
+      
+      % Compute LQR controller for unconstrained system
+      [K,Qf,~] = dlqr(mpc.A,mpc.B,Q,R);
+      % MATLAB defines K as -K, so invert its signal
+      K = -K; 
+
+      % Compute maximal invariant set
+      Xf = polytope([F;M*K],[f;m]);
+      Acl = [mpc.A + mpc.B*K];
+      while 1
+          prevXf = Xf;
+          [T,t] = double(Xf);
+          preXf = polytope(T*Acl,t);
+          Xf = intersect(Xf, preXf);
+          if isequal(prevXf, Xf)
+              break
+          end
+      end
+      [Ff,ff] = double(Xf);
+      
+      con = (x(:,2)-xs == mpc.A*(x(:,1)-xs) + mpc.B*(u(:,1)-us)) + (M*u(:,1) <= m);
+      obj = (u(:,1)-us)'*R*(u(:,1)-us);
+      for i = 2:N-1
+          con = con + (x(:,i+1)-xs == mpc.A*(x(:,i)-xs) + mpc.B*(u(:,i)-us));
+          con = con + (M*u(:,i) <= m);
+          obj = obj + (x(:,i)-xs)'*Q*(x(:,i)-xs) + (u(:,i)-us)'*R*(u(:,i)-us);
+      end
+      %con = con + (Ff*x(:,N) <= ff);
+      obj = obj + (x(:,N)-xs)'*Qf*(x(:,N)-xs);
+
+      
+      % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE 
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      
+      
+      ctrl_opt = optimizer(con, obj, sdpsettings('solver','gurobi'), ...
+        {x(:,1), xs, us, d_est}, u(:,1));
+    end
+    
+    
+    % Design a YALMIP optimizer object that takes a position reference
+    % and returns a feasible steady-state state and input (xs, us)
+    function target_opt = setup_steady_state_target(mpc)
+      
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      % INPUTS
+      %   ref    - reference to track
+      %   d_est  - disturbance estimate
+      % OUTPUTS
+      %   xs, us - steady-state target
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+      % Steady-state targets
+      n = size(mpc.A,1);
+      xs = sdpvar(n, 1);
+      us = sdpvar;
+      
+      % Reference position (Ignore this before Todo 3.3)
+      ref = sdpvar;
+      
+      % Disturbance estimate (Ignore this before Part 5)
+      d_est = sdpvar(1);
+      
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE 
+      % You can use the matrices mpc.A, mpc.B, mpc.C and mpc.D
+      con = [];
+      obj = 0;
+      % SET PARAMETERS HERE
+      Qs = 1;
+      Rs = 10;
+            
+      % CONSTRAINTS
+      %  u in U = { u | Mu <= m }
+      M = [1;-1]; m = [0.3; 0.2];
+      % x in X = { x | Fx <= f }
+      F = [0 0; 0 0]; f = [0; 0];
+      
+      %con = (xs == mpc.A*xs + mpc.B*us);
+      %con = con + (M*us <= m);
+      %obj = (mpc.C*xs - ref)'*Qs*(mpc.C*xs-ref);
+      
+      con = (xs == mpc.A*xs + mpc.B*us);
+      con = con + (M*us <= m);
+      %con = con + (F*xs <= f);
+      obj = (mpc.C*xs - ref)'*Qs*(mpc.C*xs-ref);
+      
+      % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE 
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      
+      
+      % Compute the steady-state target
+      target_opt = optimizer(con, obj, sdpsettings('solver', 'gurobi'), {ref, d_est}, {xs, us});
+    end
+    
+    
+    % Compute augmented system and estimator gain for input disturbance rejection
+    function [A_bar, B_bar, C_bar, L] = setup_estimator(mpc)
+      
+      %%% Design the matrices A_bar, B_bar, L, and C_bar
+      %%% so that the estimate x_bar_next [ x_hat; disturbance_hat ]
+      %%% converges to the correct state and constant input disturbance
+      %%%   x_bar_next = A_bar * x_bar + B_bar * u + L * (C_bar * x_bar - y);
+      
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE 
+      % You can use the matrices mpc.A, mpc.B, mpc.C and mpc.D
+      
+
+      [n,m] = size(mpc.B);
+      
+      A_bar = [];
+      B_bar = [];
+      C_bar = [];
+      
+      L = [];
+      
+      % YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE YOUR CODE HERE 
+      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    end
+
+    
+  end
+end
